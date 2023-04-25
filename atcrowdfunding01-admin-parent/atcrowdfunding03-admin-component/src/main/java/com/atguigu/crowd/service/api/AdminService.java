825916770f68fddfaca3d6b89d551fb1b66c41ab
Add5151 <L19958126849@163.com>
2023-04-25 16:23:26 +0800

    声明式事务

---
+++ atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/service/api/AdminService.java
@@ -0,0 +1,11 @@
+package com.atguigu.crowd.service.api;
+
+import com.atguigu.crowd.entity.Admin;
+
+/**
+ * @author long
+ * @create 2023-04-25 16:15
+ */
+public interface AdminService {
+    void saveAdmin(Admin admin);
+}
